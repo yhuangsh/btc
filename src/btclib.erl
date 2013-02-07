485d0aa1d41baeb711cfbfe4159b2538e8240f38
Yong Huang <yong.huang.sh@gmail.com>
2013-02-07 15:03:33 +0800

    checkin block parsing code that compiles

--- src/btclib.erl
+++ src/btclib.erl
@@ -1,14 +1,14 @@
--moduel(btclib).
+-module(btclib).
 -compile(export_all).
 
 -record(btcblk, 
 	{ magic,
 	  size, 
 	  header,
-	  n_trans,
-	  trans }).
+	  n_tx,
+	  txs }).
 
--record(btcblkheader 
+-record(btcblkhd,
 	{ version,
 	  hash_prev_blk,
 	  hash_merkle_root,
@@ -16,72 +16,142 @@
 	  bits,
 	  nonce }).
 
-parse_block(Blob) when is_binary(Blob) ->
-    parse_block_ret(
-      parse_transactions(
-	parse_block_header(
-	  chk_block_size(
-	    chk_magic({Blob, #btcblk{}}))))).
-parse_block_ret({Blob, undefined}) -> {Blob, undefined};
-parse_block_ret({Blob, Parsed}) -> 
-    NewTransList = lists:reverse(Parsed#btcblk.trans),
-    {Blob, Parsed#btcblk{trans=NewTransList}}.
+-record(btctx,
+	{ version,
+	  n_in,
+	  ins,
+	  n_out,
+	  outs,
+	  lock_time }).
 
-parse_transactions({Blob, undefined}) -> {Blob, undefined};
-parse_transactions({<<N, Blob/binary>>, Parsed}) when N < 16#fd, is_record(Parsed, btcblk) ->
-    parse_transactions(N, {Blob, Parsed#{n_trans=N}});
-parse_transactions({<<16#fd, N:16/little-unsigned-integer, Blob/binary>>, Parsed}) when is_record(Parsed, btcblk) ->
-    parse_transactions(N, {Blob, Parsed#{n_trans=N}});
-parse_transactions({<<16#fe, N:32/little-unsigned-integer, Blob/binary>>, Parsed}) when is_record(Parsed, btcblk) ->
-    parse_transactions(N, {Blob, Parsed#{n_trans=N}});
-parse_transactions({<<16#ff, Counter:64/little-unsigned-integer, Blob/binary>>, Parsed}) when is_record(Parsed, btcblk) ->
-    parse_transactions(N, {Blob, Parsed#{n_trans=N}});
-parse_transactions({Blob, _} -> {Blob, undefined}.
-			 
-parse_transactions(0, {Blob, Parsed}) -> {Blob, Parsed};
-parse_transactions(_, {Blob, undefined}) -> parse_transactions(0, {Blob, undefined});
-parse_transactions(N, {Blob, Parsed}) -> parse_transactions_ret(parse_transaction({Blob, #btctr{}})).
-parse_transactions_ret({NewBlob, undefined}) -> parse_transactions(N-1, {NewBlob, undefined});
-parse_transactions_ret({NewBlob, NewTrans}) ->
-    NewTransList = [NewTrans | Parsed#btcblk.trans],
-    parse_transactions(N-1, {NewBlob, Parsed#btcblk{trans=NewTransList}}).
+-record(btctxin,
+	{ prev_tx_hash,
+	  prev_txout_idx,
+	  script_len,
+	  script,
+	  seq }).
 
-parse_transaction({Blob, ParsedTrans}) ->
-    parse_trans_lock_time(
-      parse_trans_outputs(
-	parse_trans_n_out(
-	  parse_trans_inputs(
-	    parse_trans_n_in(
-	      parse_trans_version({Blob, #btctr{}})))))).
+-record(btctxout,
+	{ value,
+	  script_len,
+	  script }).
+
+parse_blk(Blob) when is_binary(Blob) ->
+    parse_txs(
+      parse_n_tx(
+	parse_blk_hd(
+	  chk_blk_size(
+	    chk_magic(Blob))))).
+
+chk_magic(<<16#D9B4BEF9:32/little-unsigned-integer, Blob/binary>>) -> 
+    {Blob, #btcblk{magic=16#d9b4bef9}}.
+
+chk_blk_size({<<BlockSize:4/little-unsigned-integer, Blob/binary>>, Parsed}) when BlockSize > size(Blob) ->
+    {Blob, Parsed#btcblk{size=BlockSize}}.
+
+parse_blk_hd({<<Version:32/little-unsigned, 
+		HashPrevBlock:32/bytes, 
+		HashMerkleRoot:32/bytes, 
+		Time:32/little-unsigned-integer,
+		Bits:32/little-unsigned-integer,
+		Nonce:32/little-unsigned-integer, Blob/binary>>, Parsed}) ->
+    {Blob, Parsed#btcblk{header=#btcblkhd{version = Version,
+					  hash_prev_blk = HashPrevBlock,
+					  hash_merkle_root = HashMerkleRoot,
+					  time = Time,
+					  bits = Bits,
+					  nonce = Nonce}}}.
+
+parse_n_tx({Blob, Parsed}) ->
+    {NewBlob, N} = parse_var_len(Blob),
+    {NewBlob, Parsed#btcblk{n_tx=N}}.
+    
+parse_txs({Blob, Parsed}) -> 
+    parse_txs(Parsed#btcblk.n_tx, {Blob, Parsed}).
+	    
+parse_txs(0, {Blob, Parsed}) -> 
+    TxList = lists:reverse(Parsed#btcblk.txs),
+    {Blob, Parsed#btcblk{txs=TxList}};
+parse_txs(N, {Blob, Parsed}) -> 
+    {NewBlob, Tx} = parse_tx(Blob),
+    TxList = [Tx | Parsed#btcblk.txs],
+    parse_txs(N-1, {NewBlob, Parsed#btcblk{txs=TxList}}).
+
+parse_tx(Blob) ->
+    parse_tx_lock_time(
+      parse_tx_outs(
+	parse_tx_n_out(
+	  parse_tx_ins(
+	    parse_tx_n_in(
+	      parse_tx_version(Blob)))))).
+
+parse_tx_version(<<Version:32/little-unsigned-integer, Blob/binary>>) -> 
+    {Blob, #btctx{version=Version}}.
+
+parse_tx_n_in({Blob, Parsed}) -> 
+    {NewBlob, N} = parse_var_len(Blob),
+    {NewBlob, Parsed#btctx{n_in=N}}.
+
+parse_tx_ins({Blob, Parsed}) ->
+    parse_tx_ins(Parsed#btctx.n_in, {Blob, Parsed}).
+
+parse_tx_ins(0, {Blob, Parsed}) ->
+    Ins = lists:reverse(Parsed#btctx.ins),
+    {Blob, Parsed#btctx{ins=Ins}};
+parse_tx_ins(N, {Blob, Parsed}) ->
+    {NewBlob, I} = parse_tx_in(Blob),
+    Ins = [I|Parsed#btctx.ins], 
+    parse_tx_ins(N-1, {NewBlob, Parsed#btctx{ins=Ins}}).
+    
+parse_tx_in(<<PrevTxHash:32/bytes, PrevTxOutIndex:4/little-unsigned-integer, Blob/binary>>) ->
+    {NewBlob, ScriptLen} = parse_var_len(Blob),
+    parse_tx_in_seq_no(
+      parse_tx_in_script_sig({NewBlob, #btctxin{prev_tx_hash=PrevTxHash,
+						prev_txout_idx=PrevTxOutIndex,
+						script_len=ScriptLen}})).
+
+parse_tx_in_script_sig({Blob, Parsed}) ->
+    ScriptLen = Parsed#btctxin.script_len,
+    <<Script:ScriptLen/bytes, NewBlob/binary>> = Blob,
+    {NewBlob, Parsed#btctxin{script=Script}}.
+
+parse_tx_in_seq_no({<<Seq:32/little-unsigned-integer, Blob>>, Parsed}) ->
+    {Blob, Parsed#btctxin{seq=Seq}}.
+
+parse_tx_n_out({Blob, Parsed}) -> 
+    {NewBlob, N} = parse_var_len(Blob),
+    {NewBlob, Parsed#btctx{n_out=N}}.
+
+parse_tx_outs({Blob, Parsed}) ->
+    parse_tx_outs(Parsed#btctx.n_out, {Blob, Parsed}).
+
+parse_tx_outs(0, {Blob, Parsed}) ->
+    Outs = lists:reverse(Parsed#btctx.outs),
+    {Blob, Parsed#btctx{outs=Outs}};
+parse_tx_outs(N, {Blob, Parsed}) ->    
+    {NewBlob, O} = parse_tx_out(Blob),
+    Outs = [O|Parsed#btctx.outs], 
+    parse_tx_outs(N-1, {NewBlob, Parsed#btctx{outs=Outs}}).
+    
+parse_tx_out(<<Value:64/little-unsigned-integer, Blob/binary>>) ->
+    {NewBlob, ScriptLen} = parse_var_len(Blob),
+    parse_tx_out_script_puk({NewBlob, #btctxout{value=Value, script_len=ScriptLen}}).
+
+parse_tx_out_script_puk({Blob, Parsed}) ->
+    ScriptLen = Parsed#btctxout.script_len,
+    <<Script:ScriptLen/bytes, NewBlob/binary>> = Blob, 
+    {NewBlob, Parsed#btctxout{script=Script}}.
+
+parse_tx_lock_time({<<LockTime:32/little-unsigned-integer, Blob/binary>>, Parsed}) ->
+    {Blob, Parsed#btctx{lock_time=LockTime}}.
+
+parse_var_len(<<N, Blob/binary>>) when N < 16#fd -> {Blob, N};
+parse_var_len(<<16#fd, N:16/little-unsigned-integer, Blob/binary>>) -> {Blob, N};
+parse_var_len(<<16#fe, N:32/little-unsigned-integer, Blob/binary>>) -> {Blob, N};
+parse_var_len(<<16#ff, N:64/little-unsigned-integer, Blob/binary>>) -> {Blob, N}.
 
 
-		     
-chk_magic({<<16#D9B4BEF9:32/little-unsigned, Blob/binary>>, Parsed}) when is_record(Parsed, btcblk) -> 
-    {Blob, Parsed#{magic=16#D9B4BEF9}};
-chk_magic({Blob, _}) -> {Blob, undefined}.
-
-chk_block_size({<<BlockSize:4/little-unsigned, Blob/binary>>, Parsed}) when is_record(Parsed, btcblk) ->
-    if 
-	BlockSize > size(Blob) -> {Blob, undefined};
-	true -> {Blob, Parsed#{size=BlockSize}}
-    end;
-chk_block_size(Raw, _) -> {Raw, undefined}.
-
-parse_block_header(<<Version:32/little-unsigned, 
-		     HashPrevBlock:32/bytes, 
-		     HashMerkleRoot:32/bytes, 
-		     Time:32/little-unsigned-integer,
-		     Bits:32/little-unsigned-integer,
-		     Nonce:32/little-unsigned-integer, Blob/binary>>, Parsed) when is_record(Parsed, btcblk) ->
-    {Blob, Parsed#{header=#btcblkheader{version = Version,
-					hash_prev_block = HashPrevBlock,
-					hash_merkle_root = HashMerkleRoot,
-					time = Time,
-					bits = Bits,
-					nonce = Nonce}}};
-parse_block_header(Blob, _) -> {Blob, undefined}.
-
-	
+    
           
     
 	    
